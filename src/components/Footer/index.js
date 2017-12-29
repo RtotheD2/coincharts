6f3851d8bff6bb92f13132eb538d1af6c362cb2b
Justin Toh <tohjustin@hotmail.com>
2017-12-30 01:36:08 +0800

    Component: Display app version in <Footer/>

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import logo from './assets/logo.svg';
-import './index.css';
+import logo from "./assets/logo.svg";
+import "./index.css";
+
+const APP_VERSION_LABEL = `v${process.env.REACT_APP_VERSION}`;
+const COINBASE_API_URL = "https://developers.coinbase.com/api/v2";
+const GITHUB_REPOSITORY_URL = `https://github.com/tohjustin/coincharts/releases/tag/${process.env.REACT_APP_VERSION}`;
 
 const Footer = () => (
   <div className="Footer">
-    <span>Powered by </span>
-    <a
-      href="https://developers.coinbase.com/api/v2"
-      target="_blank"
-      rel="noopener noreferrer"
-    >
-      <img
-        src={logo}
-        className="logo"
-        alt="coinbase"
-      />
-    </a>
+    <div>
+      <span>Powered by </span>
+      <a href={COINBASE_API_URL} target="_blank" rel="noopener noreferrer">
+        <img src={logo} className="logo" alt="coinbase" />
+      </a>
+    </div>
+    <div>
+      <a href={GITHUB_REPOSITORY_URL} target="_blank" rel="noopener noreferrer">
+        {APP_VERSION_LABEL}
+      </a>
+    </div>
   </div>
 );
 
